7df74791b217fd139cacf0788bdbc5a6d4f9b9d9
Ruslan Gustomiasov <rusik69@gmail.com>
2026-08-30 20:55:56 +0000

    build: regenerate users.d and sh.d dependency lists
    Add stdlib.h/stdint.h deps for users and sh build targets.

--- userspace/bin/cmds/users.d
+++ userspace/bin/cmds/users.d
@@ -1,6 +1,9 @@
 bin/cmds/users.o: bin/cmds/users.c libc/include/unistd.h \
- libc/include/stdio.h libc/include/unistd.h libc/include/string.h
+ libc/include/stdio.h libc/include/unistd.h libc/include/string.h \
+ libc/include/stdlib.h libc/include/stdint.h
 libc/include/unistd.h:
 libc/include/stdio.h:
 libc/include/unistd.h:
 libc/include/string.h:
+libc/include/stdlib.h:
+libc/include/stdint.h:
--- userspace/bin/sh.d
+++ userspace/bin/sh.d
@@ -1,9 +1,10 @@
 bin/sh.o: bin/sh.c libc/include/stdarg.h libc/include/stdio.h \
- libc/include/unistd.h libc/include/stdlib.h libc/include/string.h \
- libc/include/unistd.h
+ libc/include/unistd.h libc/include/stdlib.h libc/include/stdint.h \
+ libc/include/string.h libc/include/unistd.h
 libc/include/stdarg.h:
 libc/include/stdio.h:
 libc/include/unistd.h:
 libc/include/stdlib.h:
+libc/include/stdint.h:
 libc/include/string.h:
 libc/include/unistd.h:
